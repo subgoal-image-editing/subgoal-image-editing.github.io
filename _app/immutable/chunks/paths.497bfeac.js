d27389658412d3ae98351f88c89c071aba72cc69
subgoal-image-editing <kevin3.black+++@gmail.com>
2023-10-23 18:46:59 -0700

    Updates

--- _app/immutable/chunks/paths.497bfeac.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_txwf24)==null?void 0:s.base)??"/susie";var t;const a=((t=globalThis.__sveltekit_txwf24)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/chunks/paths.f405ed7e.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1n47kfq)==null?void 0:s.base)??"/susie";var e;const t=((e=globalThis.__sveltekit_1n47kfq)==null?void 0:e.assets)??a;export{t as a,a as b};
